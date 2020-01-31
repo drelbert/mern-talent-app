6cb9431abf49d0acf522f804b95d8c101ba32644
Elbert <DElbert@HRSA.Gov>
2020-01-31 08:52:49 -0500

    friday push

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import BarChart from './charts/BarChart';
 
 import './Dashboard.css';
 
-const Dashboard = () => {
+class Dashboard extends Component {
 
-const data = [ "one", "dos", "tres" ]
-const divs = data.map((d,i) => <div key={i}> {d}</div>)
-
-const wrap = <div style={{ marginLeft: "20px" }}
-    className="wrapper">{divs}</div>
-
-return (
-    <div>{wrap}</div>
-)
-};
+  render() {
+    return (
+        <div>
+        <div>
+          <h2>d3ia dashboard</h2>
+        </div>
+        <div>
+        <BarChart data={[5,10,1,3]} size={[500,500]} />
+        </div>
+      </div>
+     )
+    };
+}
 
 export default Dashboard;
 
